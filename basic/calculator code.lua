82bc73d109f8b85a02ac8997bacafb8407abcaf7
Andrew Seligman <wilshie@gmail.com>
2021-11-26 14:31:12 -0500

    Refactor and add logic for deleted objects

--- basic/calculator code.lua
+++ basic/calculator code.lua
@@ -26,47 +26,50 @@ function onload(scriptState)
         self.UI.setAttributes("status", {["text"]="Enabled", ["color"]="Green"})
         self.UI.setAttributes(uiID, {["text"]="Click to disable", ["isOn"]=true})
     end
-
     populateTables()
-    --Track the results of each die rolled by a specific player
-    function onObjectRandomize(obj, color)
-        if(not isActive or obj.getValue() == nil) then return end
-        --Ignore non-Dice objects and non-numerical dice
-        if(obj.tag ~= "Dice" or tonumber(obj.getRotationValue()) == nil) then return end
-        --Prevent adding the same object to the isWatched table more than once
-        --(A die can be randomized multiple times before coming to rest)
-        local id = obj.getGUID()
-        local exists = false
-        for k, v in pairs(isWatched[color]) do
-            if(v == id) then
-                exists = true
-                break
-            end
+end
+
+--Track the results of each die rolled by a specific player
+function onObjectRandomize(obj, color)
+    if(not isActive or obj.getValue() == nil) then return end
+    --Ignore non-Dice objects and non-numerical dice
+    if(obj.tag ~= "Dice" or tonumber(obj.getRotationValue()) == nil) then return end
+    --Prevent adding the same object to the isWatched table more than once
+    --(A die can be randomized multiple times before coming to rest)
+    local id = obj.getGUID()
+    local exists = false
+    for k, v in pairs(isWatched[color]) do
+        if(v == id) then
+            exists = true
+            break
         end
-        if(not exists) then
-            table.insert(isWatched[color], id)
-            local function rollFinished()
-                local result = obj.getRotationValue()
-                if(tonumber(result) ~= nil) then
-                    table.insert(trackDice[color], {#obj.getRotationValues(), result})
-                    rollTotal[color] = rollTotal[color] + result
+    end
+    if(not exists) then
+        table.insert(isWatched[color], id)
+        local function rollFinished()
+            local result = obj.getRotationValue()
+            if(tonumber(result) ~= nil and not obj.isDestroyed()) then
+                table.insert(trackDice[color], {#obj.getRotationValues(), result})
+                rollTotal[color] = rollTotal[color] + result
+            end
+            for k, v in pairs(isWatched[color]) do
+                if(v == id) then
+                    table.remove(isWatched[color], k)
+                    break
                 end
-                for k, v in pairs(isWatched[color]) do
-                    if(v == id) then
-                        table.remove(isWatched[color], k)
-                        break
-                    end
-                end
+            end
             endRoll(color, #isWatched[color])
-            end
-            Wait.condition(rollFinished, || obj.resting)
         end
+        local function dieStopped()
+            return obj.isDestroyed() or obj.resting
+        end
+        Wait.condition(rollFinished, dieStopped)
     end
 end
 
 --Do stuff when the last die comes to rest
 function endRoll(color, listSize)
-    if(listSize == 0) then
+    if(listSize == 0 and #trackDice[color] > 0) then
         --Organize type of dice in tallyDice
         local tallyString, resultString = "", ""
         table.sort(trackDice[color], function(a,b) return a[1]>b[1] end) --Decreasing order
